f2d0cdf7c303f1089794c3b43e0c99d217ce5381
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-07-24 15:53:19 -0700

    setup grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,29 @@
-import Image from "next/image";
+"use client";
+
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-10 overflow-x-hidden">
-      <section className="grid grid-cols-3 p-10 gap-10">
-        <div className="bg-slate-800 aspect-sqaure rounded-lg justify-center flex items-center gap-10"></div>
-        <div className="bg-slate-800 aspect-sqaure rounded-lg justify-center flex items-center gap-10"></div>
-        <div className="bg-slate-800 aspect-sqaure rounded-lg justify-center flex items-center gap-10"></div>
-        <div className="bg-slate-800 aspect-sqaure rounded-lg justify-center flex items-center gap-10"></div>
-        <div className="bg-slate-800 aspect-sqaure rounded-lg justify-center flex items-center gap-10"></div>
-        <div className="bg-slate-800 aspect-sqaure rounded-lg justify-center flex items-center gap-10"></div>
-      </section>
+      <motion.section
+        variants={{
+          hidden: { opacity: 0 },
+          show: {
+            opacity: 1,
+            transition: {
+              staggerChildren: 0.25,
+            },
+          },
+        }}
+        className="grid grid-cols-3 p-10 gap-10"
+      >
+        <div className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></div>
+        <div className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></div>
+        <div className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></div>
+        <div className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></div>
+        <div className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></div>
+        <div className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></div>
+      </motion.section>
     </div>
   );
 }
