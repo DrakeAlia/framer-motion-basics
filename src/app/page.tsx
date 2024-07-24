1b7dca83489ac2f214740a6c223b5c65e29e9446
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-07-24 16:13:14 -0700

    added motion to all the square divs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,55 @@
 
 import { motion } from "framer-motion";
 
+const gridContainerVariants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.25,
+    },
+  },
+};
+
+const gridSquareVariants = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
+
 export default function Home() {
   return (
     <div className="flex flex-col gap-10 overflow-x-hidden">
       <motion.section
-        variants={{
-          hidden: { opacity: 0 },
-          show: {
-            opacity: 1,
-            transition: {
-              staggerChildren: 0.25,
-            },
-          },
-        }}
+        variants={gridContainerVariants}
+        initial="hidden"
+        animate="show"
         className="grid grid-cols-3 p-10 gap-10"
       >
-        <div className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></div>
-        <div className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></div>
-        <div className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></div>
-        <div className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></div>
-        <div className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></div>
-        <div className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"></div>
+        {/* Fade Up */}
+        <motion.div
+          variants={gridSquareVariants}
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        ></motion.div>
+        <motion.div
+          variants={gridSquareVariants}
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        ></motion.div>
+        <motion.div
+          variants={gridSquareVariants}
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        ></motion.div>
+        <motion.div
+          variants={gridSquareVariants}
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        ></motion.div>
+        <motion.div
+          variants={gridSquareVariants}
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        ></motion.div>
+        <motion.div
+          variants={gridSquareVariants}
+          className="bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10"
+        ></motion.div>
       </motion.section>
     </div>
   );
